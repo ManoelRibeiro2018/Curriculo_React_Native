4322607020b52df292d85c96d158a5ca48e7d585
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-12-12 15:22:38 -0300

    Criando curriculo mokado

---
+++ src/data/curriculo.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        name: 'Manoel Ribeiro',
+        email: 'manoel@gmail.com',
+        AvataUrl : 'https://cdn.pixabay.com/photo/2021/07/03/10/54/naruto-6383641_960_720.png'
+    },
+    {
+        id: 1,
+        name: 'Manoel One',
+        email: 'manoel@gmail.com',
+        AvataUrl : 'https://cdn.pixabay.com/photo/2021/07/03/10/54/naruto-6383641_960_720.png'
+    },
+    {
+        id: 1,
+        name: 'Manoel Two',
+        email: 'manoel@gmail.com',
+        AvataUrl : 'https://cdn.pixabay.com/photo/2021/07/03/10/54/naruto-6383641_960_720.png'
+    }
+]
